8e6cfb191b2929740abff157784533cd255d8d9d
林哲暐 <chewei@Kerwin-de-MacBook-Air.local>
2022-12-24 02:44:50 +0800

    add "title" filed

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -1,18 +1,23 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-  user_id: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
-  class_id: { type: mongoose.Types.ObjectId, ref: 'Class', required: true },
-  content: { type: String },
-  condition: { type: String },
-  deadline: { type: Date },
-  tag: [{ type: String }],
-}, {
-  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
-});
+// 新增 title
+const PostSchema = new Schema(
+  {
+    user_id: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    title: { type: String }, 
+    class_id: { type: mongoose.Types.ObjectId, ref: "Class", required: true },
+    content: { type: String },
+    condition: { type: String },
+    deadline: { type: Date },
+    tag: [{ type: String }],
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
 
-const PostModel = mongoose.model('Post', PostSchema);
+const PostModel = mongoose.model("Post", PostSchema);
 
 export { PostModel };
