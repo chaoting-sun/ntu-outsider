80d17a44c87b4c30bc36ba8d9802929ec72a779f
林哲暐 <chewei@Kerwin-de-MacBook-Air.local>
2022-12-28 21:43:17 +0800

    temporarily remove subscription

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,27 @@
-import * as fs from 'fs';
-import { createServer } from 'node:http';
-import { WebSocketServer } from 'ws';
-import { createPubSub, createSchema,createYoga } from 'graphql-yoga';
-import { useServer } from 'graphql-ws/lib/use/ws';
-import {ChatBoxModel} from './models/chatbox';
-import { ClassModel } from './models/class';
-import { CommentModel } from './models/comment';
-import { PostModel } from './models/post';
-import { UserModel } from './models/user';
-import Query from './resolvers/Query';
-import Mutation from './resolvers/Mutation';
-import Subscription from './resolvers/Subscription';
-import ChatBox from './resolvers/ChatBox';
+import * as fs from "fs";
+import { createServer } from "node:http";
+import { WebSocketServer } from "ws";
+import { createPubSub, createSchema, createYoga } from "graphql-yoga";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { ChatBoxModel } from "./models/chatbox";
+import { ClassModel } from "./models/class";
+import { CommentModel } from "./models/comment";
+import { PostModel } from "./models/post";
+import { UserModel } from "./models/user";
+import Query from "./resolvers/Query";
+import Mutation from "./resolvers/Mutation";
+// import Subscription from "./resolvers/Subscription";
+import ChatBox from "./resolvers/ChatBox";
 const pubsub = createPubSub();
-
 
 const yoga = new createYoga({
   schema: createSchema({
-    typeDefs: fs.readFileSync('./src/schema.graphql', 'utf-8' ),
+    typeDefs: fs.readFileSync("./src/schema.graphql", "utf-8"),
     resolvers: {
       Query,
       Mutation,
-      Subscription,
-      ChatBox
+      // Subscription,
+      ChatBox,
     },
   }),
   context: {
@@ -34,47 +33,45 @@ const yoga = new createYoga({
     pubsub,
   },
   graphql: {
-      subscriptionsProtocol: 'WS',
-  }
- 
+    subscriptionsProtocol: "WS",
+  },
 });
 
-const httpServer = createServer(yoga)
+const httpServer = createServer(yoga);
 const wsServer = new WebSocketServer({
   server: httpServer,
   path: yoga.graphqlEndpoint,
-})
+});
 
- 
 useServer(
   {
-  execute: (args) => args.rootValue.execute(args),
-  subscribe: (args) => args.rootValue.subscribe(args),
-  onSubscribe: async (ctx, msg) => {
-    const { schema, execute, subscribe, contextFactory, parse, validate } =
-      yoga.getEnveloped({
-        ...ctx,
-        req: ctx.extra.request,
-        socket: ctx.extra.socket,
-        params: msg.payload
-      })
-    const args = {
-      schema,
-      operationName: msg.payload.operationName,
-      document: parse(msg.payload.query),
-      variableValues: msg.payload.variables,
-      contextValue: await contextFactory(),
-      rootValue: {
-        execute,
-        subscribe
-      }
-    }
-    const errors = validate(args.schema, args.document)
-    if (errors.length) return errors
-    return args
-  }, 
-},
-  wsServer, 
-)
-    
+    execute: (args) => args.rootValue.execute(args),
+    subscribe: (args) => args.rootValue.subscribe(args),
+    onSubscribe: async (ctx, msg) => {
+      const { schema, execute, subscribe, contextFactory, parse, validate } =
+        yoga.getEnveloped({
+          ...ctx,
+          req: ctx.extra.request,
+          socket: ctx.extra.socket,
+          params: msg.payload,
+        });
+      const args = {
+        schema,
+        operationName: msg.payload.operationName,
+        document: parse(msg.payload.query),
+        variableValues: msg.payload.variables,
+        contextValue: await contextFactory(),
+        rootValue: {
+          execute,
+          subscribe,
+        },
+      };
+      const errors = validate(args.schema, args.document);
+      if (errors.length) return errors;
+      return args;
+    },
+  },
+  wsServer
+);
+
 export default httpServer;
