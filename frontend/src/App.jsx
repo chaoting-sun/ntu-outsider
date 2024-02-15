8ee890598c08cbdc44f45480df3100103b02a3fa
chaoting-sun <chaoting0108@gmail.com>
2024-02-15 17:46:18 +0800

    refactor: revise React routing by react-router-dom v6.4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,61 @@
-import './css/App.css';
+import "./css/App.css";
 
-import { React } from 'react'
-import { OutsiderProvider } from './containers/hooks/useOusider';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPage from './containers/mainPage';
-import SignInPage from './containers/signInPage';
-import Navbar from './components/navigationBar';
-import EditPostPage from './containers/editPostPage';
-import MailPage from './containers/mailPage';
-import MyProfilePage from './containers/myProfilePage';
-import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { React } from "react";
+import { OutsiderProvider } from "./containers/hooks/useOusider";
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import MainPage from "./containers/mainPage";
+import SignInPage from "./containers/signInPage";
+import Navbar from "./components/navigationBar";
+import EditPostPage from "./containers/editPostPage";
+import MailPage from "./containers/mailPage";
+import MyProfilePage from "./containers/myProfilePage";
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
+
+const router = createBrowserRouter([
+  {
+    path: "/logIn",
+    element: <SignInPage />,
+  },
+  {
+    element: <Navbar />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />,
+        index: true,
+      },
+      {
+        path: "/editPostPage",
+        element: <EditPostPage />,
+      },
+      {
+        path: "/mailPage",
+        element: <MailPage />,
+      },
+      {
+        path: "/myProfilePage",
+        element: <MyProfilePage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <OutsiderProvider>
       <ThemeProvider theme={theme}>
-        <Router>
-          <Routes>
-            <Route path="/logIn" element={<SignInPage />} />
-            <Route path='/' element={<Navbar />}>
-              <Route index element={<MainPage />} />
-              <Route path="/editPostPage" element={<EditPostPage />} />
-              <Route path="/mailPage" element={<MailPage />} />
-              <Route path="/myProfilePage" element={<MyProfilePage />} />
-            </Route>
-          </Routes>
-        </Router>
+        <RouterProvider router={router} />
       </ThemeProvider>
     </OutsiderProvider>
-  )
+  );
 }
 
 export default App;
